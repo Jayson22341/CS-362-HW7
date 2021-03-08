6a3c44418cc7c3a2076b00a719b9922737c68af5
Jayson <kildeaj@oregonstate.edu>
2021-03-08 15:53:33 -0800

    3rd test: check for %5, return "Buzz"

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -8,6 +8,9 @@ class FizzBuzzTest(unittest.TestCase):
     def test_three(self):
         result = FizzBuzz.FizzBuzz(3)
         self.assertEqual(result,"Fizz")
+    def test_five(self):
+        result = FizzBuzz.FizzBuzz(5)
+        self.assertEqual(result,"Buzz")
 
 if __name__ == "__main__":
     unittest.main()
