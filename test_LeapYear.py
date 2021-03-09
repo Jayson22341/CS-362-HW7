d9d8929639b38bb6e4378ab93d6e0e57f3fe7282
Jayson <kildeaj@oregonstate.edu>
2021-03-08 18:21:29 -0800

    added 3rd test, tests if %100==false

--- test_LeapYear.py
+++ test_LeapYear.py
@@ -8,6 +8,9 @@ class LeapYearTest(unittest.TestCase):
     def test_four(self):
         result = LeapYear.YearCheck(4)
         self.assertEqual(result, True)
+    def test_hundred(self):
+        result = LeapYear.YearCheck(100)
+        self.assertEqual(result, False)
 
 if __name__ == "__main__":
     unittest.main()
