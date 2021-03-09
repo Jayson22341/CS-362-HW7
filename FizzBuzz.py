3553b2b58c72a7fe523cadae4c52b9d16547b0fe
Jayson <kildeaj@oregonstate.edu>
2021-03-08 16:13:45 -0800

    FizzBuzz now passes All 4 tests, program complete!

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,10 +1,13 @@
 
 
 def FizzBuzz(x):
-    if x % 3 == 0:
-        return "Fizz"
-    if x % 5 == 0:
-        return "Buzz"
+    if x % 3 == 0 or x % 5 == 0:
+        returnstring = ""
+        if x % 3 == 0:
+            returnstring += "Fizz"
+        if x % 5 == 0:
+            returnstring += "Buzz"
+        return returnstring
     return x
 
 print("This is the FizzBuzz Program! I'm gonna do my thing!")
