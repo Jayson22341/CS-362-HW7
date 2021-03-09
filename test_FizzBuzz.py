31ca00c18c499fc04cfb9cf1bec54658b3593f6e
Jayson <kildeaj@oregonstate.edu>
2021-03-08 16:05:46 -0800

    added 4th test: check for %3 & %5

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -11,6 +11,9 @@ class FizzBuzzTest(unittest.TestCase):
     def test_five(self):
         result = FizzBuzz.FizzBuzz(5)
         self.assertEqual(result,"Buzz")
+    def test_three_and_five(self):
+        result = FizzBuzz.FizzBuzz(15)
+        self.assertEqual(result,"FizzBuzz")
 
 if __name__ == "__main__":
     unittest.main()
