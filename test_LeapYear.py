8ff1e683da9fa2f101823314e546b8c835eb6078
Jayson <kildeaj@oregonstate.edu>
2021-03-08 18:14:13 -0800

    added 2nd test, checks if %4 years are leap years

--- test_LeapYear.py
+++ test_LeapYear.py
@@ -5,6 +5,9 @@ class LeapYearTest(unittest.TestCase):
     def test_base(self):
         result = LeapYear.YearCheck(1)
         self.assertEqual(result, False)
+    def test_four(self):
+        result = LeapYear.YearCheck(4)
+        self.assertEqual(result, True)
 
 if __name__ == "__main__":
     unittest.main()
