39840ea347bcf3cc4d7bf5b39a2c6db3efc91f20
Jayson <kildeaj@oregonstate.edu>
2021-03-08 18:28:46 -0800

    program checks for %400, now returns true if == 0

--- LeapYear.py
+++ LeapYear.py
@@ -1,6 +1,8 @@
 def YearCheck(x):
     if x % 4 == 0:
         if x % 100 == 0:
+            if x % 400 == 0:
+                return True
             return False
         return True
     return False
